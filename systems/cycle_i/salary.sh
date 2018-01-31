7e5083bd731eacf73c2faa40cbbde0e8a48c7201
Aswin Karuvally <aswinbabuk@gmail.com>
2018-01-31 13:02:22 +0530

    fixed bug

--- systems/cycle_i/salary.sh
+++ systems/cycle_i/salary.sh
@@ -18,7 +18,7 @@ fi
 
 if (( $1 >= 1500 ))
 then
-    gross_salary=`echo "scale=2; $1 + ($1 + 500 + ($1*(98/100)))" | bc`
+    gross_salary=`echo "scale=2; $1 + 500 + ($1*(98/100))" | bc`
     echo "gross salary is $gross_salary"
     exit
 fi
