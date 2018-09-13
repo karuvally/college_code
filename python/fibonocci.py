7cb325a737d206c3ae544401f55140d2e7f50d34
Aswin Karuvally <aswinbabuk@gmail.com>
2018-09-13 15:44:20 +0530

    added fibonocci

--- python/fibonocci.py
+++ python/fibonocci.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 # fibonocci series
+
+# serious stuff
+import sys
+
+# variables
+previous_value = 0
+current_value = 1
+limit = int(input("limit> "))
+
+# fibonocci
+if limit == 0:
+    sys.exit()
+
+if limit >= 1:
+    print(previous_value, end = " ")
+
+if limit > 1:
+    for i in range(1, limit):
+        tmp = current_value
+        current_value += previous_value
+        previous_value = tmp
+
+        print(previous_value, end = " ")
